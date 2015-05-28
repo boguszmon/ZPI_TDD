874a652f8b4548ee77011d70a83d8212edcc6ac8
Monika <bogusz.mon@gmail.com>
2015-05-28 20:41:34 +0200

    Test 5. Sprawdzenie metody least_common_multiple - Test failed

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from exercise5 import LeastCommonMultipleOfNumbers
 
 class TestLeastCommonMultipleOfNumbers(unittest.TestCase):
 
-    def testLeastCommonMultiple(self):
+    def testLeastCommonMultipleOfNumbers(self):
         result_lcm = LeastCommonMultipleOfNumbers()
 
     def testLeastCommonMultipleHasInitialDivisible(self):
@@ -16,6 +16,10 @@ class TestLeastCommonMultipleOfNumbers(unittest.TestCase):
     def testGreatestCommonDivisor(self):
         result_lcm = LeastCommonMultipleOfNumbers()
         assert result_lcm.greatest_common_divisor(4, 6) == 2, "result_lcm is not correct"
+
+    def testLeastCommonMultiple(self):
+        result_lcm = LeastCommonMultipleOfNumbers()
+        assert result_lcm.least_common_multiple(4, 6) == 12, "result_lcm is not correct"
 
 
 if __name__ == "__main__":
