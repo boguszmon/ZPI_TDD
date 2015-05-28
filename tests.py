61cdfad706817020cf53697e8db5013e0e68fafc
Monika <bogusz.mon@gmail.com>
2015-05-28 20:27:46 +0200

    Test 3. Sprawdzenie istnienia atrybutu divisible - Test failed

--- tests.py
+++ tests.py
@@ -9,6 +9,10 @@ class TestLeastCommonMultipleOfNumbers(unittest.TestCase):
     def testLeastCommonMultiple(self):
         result_lcm = LeastCommonMultipleOfNumbers()
 
+    def testLeastCommonMultipleHasInitialDivisible(self):
+        result_lcm = LeastCommonMultipleOfNumbers()
+        assert result_lcm.divisible is not None, "result_lcm divisible is None"
+
 
 if __name__ == "__main__":
     unittest.main()
