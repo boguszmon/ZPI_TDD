3a2d82981ee140c54507302707afc6d56cbdfa3b
Monika <bogusz.mon@gmail.com>
2015-05-28 20:53:43 +0200

    Uporzadkowanie testow. Wykorzystanie metody setUp

--- tests.py
+++ tests.py
@@ -6,24 +6,20 @@ from exercise5 import LeastCommonMultipleOfNumbers
 
 class TestLeastCommonMultipleOfNumbers(unittest.TestCase):
 
-    def testLeastCommonMultipleOfNumbers(self):
-        result_lcm = LeastCommonMultipleOfNumbers()
+    def setUp(self):
+        self.result_lcm = LeastCommonMultipleOfNumbers()
 
     def testLeastCommonMultipleHasInitialDivisible(self):
-        result_lcm = LeastCommonMultipleOfNumbers()
-        assert result_lcm.divisible is not None, "result_lcm divisible is None"
+        assert self.result_lcm.divisible is not None, "result_lcm divisible is None"
 
     def testGreatestCommonDivisor(self):
-        result_lcm = LeastCommonMultipleOfNumbers()
-        assert result_lcm.greatest_common_divisor(4, 6) == 2, "result_lcm is not correct"
+        assert self.result_lcm.greatest_common_divisor(4, 6) == 2, "result_lcm is not correct"
 
     def testLeastCommonMultiple(self):
-        result_lcm = LeastCommonMultipleOfNumbers()
-        assert result_lcm.least_common_multiple(4, 6) == 12, "result_lcm is not correct"
+        assert self.result_lcm.least_common_multiple(4, 6) == 12, "result_lcm is not correct"
 
     def testFindDivisibleByIntegers(self):
-        result_lcm = LeastCommonMultipleOfNumbers()
-        assert result_lcm.find_divisible_by_numbers(10) == 2520, "result_lcm is not correct"
+        assert self.result_lcm.find_divisible_by_numbers(10) == 2520, "result_lcm is not correct"
 
 
 if __name__ == "__main__":
