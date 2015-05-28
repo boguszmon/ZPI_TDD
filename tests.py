b8e6ff3f8b8b3d0c5d39cfea997db5e645c22b12
Monika <bogusz.mon@gmail.com>
2015-05-28 20:08:23 +0200

    Test 1. Sprawdzenie istnienia klasy LeastCommonMultipleOfNumbers - Test failed

---
+++ tests.py
@@ -0,0 +1,12 @@
+__author__ = 'Monika'
+
+import unittest
+
+class TestLeastCommonMultipleOfNumbers(unittest.TestCase):
+
+    def testLeastCommonMultiple(self):
+        result_lcm = LeastCommonMultipleOfNumbers()
+
+
+if __name__ == "__main__":
+    unittest.main()
