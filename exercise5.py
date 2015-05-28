3f9d2258b08c853dc89f9de5e40a7463fe8ab0d1
Monika <bogusz.mon@gmail.com>
2015-05-28 20:38:24 +0200

    Zaimplementowanie metody greatest_common_divisor. Test 4. - passed

--- exercise5.py
+++ exercise5.py
@@ -3,3 +3,9 @@ __author__ = 'Monika'
 class LeastCommonMultipleOfNumbers():
     def __init__(self):
         self.divisible = 1
+
+    @staticmethod
+    def greatest_common_divisor(a, b):
+        while b:
+            a, b = b, a % b
+        return a
