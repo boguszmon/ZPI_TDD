2e9c3d975051feeb04d035ad5abd9c5a4e05470e
Monika <bogusz.mon@gmail.com>
2015-05-28 20:47:55 +0200

    Zaimplementowanie metody find_divisible_by_numbers. Test 6. - passed

--- exercise5.py
+++ exercise5.py
@@ -12,3 +12,8 @@ class LeastCommonMultipleOfNumbers():
 
     def least_common_multiple(self, a, b):
         return int(a * b / self.greatest_common_divisor(a, b))
+
+    def find_divisible_by_numbers(self, max_divisor):
+        for i in range(2, max_divisor + 1):
+            self.divisible = self.least_common_multiple(i, self.divisible)
+        return self.divisible
